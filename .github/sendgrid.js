e27ee65ed8d6c2300d3559fe3646aa694f517efa
Matt Kaufman <the1mattkaufman@gmail.com>
2020-06-05 12:07:31 -0700

    Update sendgrid.js

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -3,25 +3,27 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const fs = require('fs'),
-    filename = 'hello-world.pdf',
-    fileType = 'application/pdf',
-    data = fs.readFileSync('attachments/' + filename);
+const DISPLAY_URL_JSON = JSON.parse(process.env.DISPLAY_URL_JSON);
+const DISPLAY_USER_JSON = JSON.parse(process.env.DISPLAY_USER_JSON);
+const DISPLAY_USER_STRING = process.env.DISPLAY_USER_JSON;
+
+const TEXT = `
+Here's your personal scratch org info.
+You can open your org at ${DISPLAY_URL_JSON.result.url}.
+Here are some other details of your org.
+Org ID: ${DISPLAY_USER_JSON.result.orgId}
+Username: ${DISPLAY_USER_JSON.result.username}
+Instance URL: ${DISPLAY_USER_JSON.result.instanceUrl}
+Login URL: ${DISPLAY_USER_JSON.result.loginUrl}
+
+${DISPLAY_USER_STRING}
+`;
 
 const msg = {
-    to: 'peterevans.dev@protonmail.com',
-    from: 'sender@example.org',
-    subject: 'Hello world',
-    text: 'Hello plain world!',
-    html: '<p>Hello HTML world!</p>',
-    attachments: [
-        {
-            content: data.toString('base64'),
-            filename: filename,
-            type: fileType,
-            disposition: 'attachment',
-        },
-    ],
+    to: 'mkaufman@mkpartners.com',
+    from: 'mkaufman@mkpartners.com',
+    subject: 'Welcome to your new scratch org',
+    text: TEXT,
 };
 
 sgMail
